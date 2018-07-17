355ce4a64b74f98ac0990ba7c8c9ab0b4af4962b
Leonardo Duran <leonardoduran1984@gmail.com>
2018-07-16 23:47:01 -0300

    CORS

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const indexBedin = require('./routes/bedin/index');
 const indexHealthcare = require('./routes/healthcare/index');
 const indexHospital = require('./routes/hospital/index')
 const patient = require('./routes/patient')
+const cors = require('cors');
 const app = express();
 require('./config/mongoose')
 require('./config/passport-mongoose')(app);
@@ -18,14 +19,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use(express.static('./dist'));
-// app.use(express.static('../bedInFrontEnd/dist'));
-const cors = require('./controladores/cors');
+// app.use(express.static('./dist'));
+app.use(express.static('../bedInFrontEnd/dist'));
+// const cors = require('./controladores/cors');
 app.use('/', index);
 app.use('/bedin', indexBedin);
 app.use('/healthcare', indexHealthcare);
 app.use('/hospital', indexHospital);
 app.use('/patient', patient);
+app.use(cors());
 const controllerHealthcare = require('./controladores/healthcare');
 controllerHealthcare.setPatientTimeOut();
 //app.use('/bedin/healthcares', healthcares)
